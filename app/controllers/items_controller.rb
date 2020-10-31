0d8152d383ca3c419bbf96f9c10c12c7e1abed33
yakko-chn <o.ub5ryno16i@gmail.com>
2020-10-31 09:58:37 +0900

    編集機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
+    before_action :authenticate_user!, only: [:edit,:create]
+    before_action :move_to_top_page, only: [:edit]
+
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,8 +24,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  
+  end
+
   private
   def item_params
     params.require(:item).permit(:name,:text,:category_id,:item_status_id,:delivery_charge_id,:prefecture_id,:shopping_day_id,:price,:image).merge(user_id: current_user.id)
   end
+
+  def move_to_top_page
+    @item = Item.find(params[:id])
+
+   if user_signed_in? && @item.user_id != current_user.id 
+      redirect_to  action: :index
+    end
+  end
+
 end

71e36592c1b63b10179340b9f1ffd484fceeea3e
yakko-chn <o.ub5ryno16i@gmail.com>
2020-11-04 17:36:36 +0900

    修正
    ログアウトユーザーへの制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:show, :edit, :create]
+  before_action :authenticate_user!, only: [:new, :edit, :create]
   before_action :move_to_top_page, only: [:edit, :destroy]
   before_action :set_item, only: [:edit, :show, :update]
 
